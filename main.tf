93d6573bf5605ed458f556a5769fd4cbf98e267f
Максим Котлярчук <maksimkotlarcuk@MacBook-Pro-Maksim.local>
2025-05-08 17:58:23 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 data "aws_vpc" "default" {
@@ -7,40 +7,40 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_security_group" "web_server_sg_tf" {
-    name       = "web_server_sg_tf"
-    description = "Allow HTTP and SSH"
-    vpc_id      = data.aws_vpc.default.id
+  name        = "web_server_sg_tf"
+  description = "Allow HTTP and SSH"
+  vpc_id      = data.aws_vpc.default.id
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["176.107.194.143/32"]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["176.107.194.143/32"]
+  }
 
-    ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "web_server" {
-    ami                         = "ami-084568db4383264d4"
-    instance_type               = "t2.micro"
-    key_name                    = "keylab4"
-    
-    vpc_security_group_ids = [aws_security_group.web_server_sg_tf.id] 
+  ami           = "ami-084568db4383264d4"
+  instance_type = "t2.micro"
+  key_name      = "keylab4"
 
-    tags = {
-        Name = "Instance Lab6"
-    }
+  vpc_security_group_ids = [aws_security_group.web_server_sg_tf.id]
+
+  tags = {
+    Name = "Instance Lab6"
+  }
 }
