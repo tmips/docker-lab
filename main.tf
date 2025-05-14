cf866b9aace880fa92f0f94c7f974b30129a1761
Максим Котлярчук <maksimkotlarcuk@MacBook-Pro-Maksim.local>
2025-05-15 01:25:01 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,40 +48,33 @@ resource "aws_instance" "web_server" {
     #!/bin/bash
     set -e
 
-    # Update system
-    apt-get update -y
-    apt-get install -y ca-certificates curl gnupg git
-
-    # Add Docker’s official GPG key
-    install -m 0755 -d /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    chmod a+r /etc/apt/keyrings/docker.gpg
-
-    # Add Docker repo
+    sudo apt-get update
+    sudo apt-get install ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg lsb-release
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+    sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
-      https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
-      tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-    # Install Docker
-    apt-get update -y
-    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-    # Clone your repo (replace if needed)
-    cd /home/ubuntu
+    "deb [arch=$(dpkg --print-architecture) \
+    signed-by=/etc/apt/keyrings/docker.gpg] \
+    https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io \
+    docker-buildx-plugin docker-compose-plugin
     git clone https://github.com/tmips/docker-lab.git
     cd docker-lab
-
-    # Build and run container
+    sudo usermod -aG docker $USER
+    newgrp docker
     docker build -t lab4 .
     docker run -d -p 80:80 --name lab4_cont lab4
-
-    # Run Watchtower
-    docker run -d \
-      --name watchtower \
-      --restart always \
-      -v /var/run/docker.sock:/var/run/docker.sock \
-      containrrr/watchtower \
-      --interval 30 --cleanup
+    docker run -d   --name watchtower   --restart always   -v /var/run/docker.sock:/var/run/docker.sock   containrrr/watchtower   --interval 3
+   
   EOF
 }
